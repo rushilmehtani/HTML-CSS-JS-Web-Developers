88cb64ebd49fdff897c8cb44fe34e28b4ec5d440
Rushil Mehtani <rushilmehtani@Rushils-MacBook-Pro.local>
2018-10-14 18:03:18 +0530

    Module 5 Solution

--- Module-4-Solution/script.js
+++ Module-4-Solution/script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Faraz", "Rohan", "Fahad", "Karan", "Hitansh", "Shreyans", "Abhimeet", "Mohit", "Pranav", "Garvit"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
